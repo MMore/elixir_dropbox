e1cd2512d2e8217aaedab75688891dd1193f0ddc
Steve Forkin <steve@netflakes.co>
2018-11-28 10:07:35 +1100

    updated the version number

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ElixirDropbox.Mixfile do
 
   def project do
     [app: :elixir_dropbox,
-     version: "0.0.10",
+     version: "0.1.1",
      elixir: "~> 1.6",
      name: "ElixirDropbox",
      description: @description,
